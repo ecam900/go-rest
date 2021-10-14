468736219b92fa2fd3d211e20dc60b237f5c93c9
eric c <ecam900@gmail.com>
2021-10-14 01:25:34 -0400

    Add app struct.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+// App struct which contains things like pointers
+// to database connections
+type App struct {}
+
+// Run - Sets up  our application 
+func (app *App) Run() error {
+	fmt.Println("Setting Up...")
+	return nil
+}
+
 func main() {
 	fmt.Println("Prod Ready Rest API Boilerplate")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error Starting Up")
+
+	}
 }
